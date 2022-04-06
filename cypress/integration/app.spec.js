153ce4efb6119d7770f5dad28efe8235eefd6ef1
edemuner <eduardood15@hotmail.com>
2022-04-06 20:45:25 -0300

    created a test to check if all three added itens were really added and were added in the correct order

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -20,4 +20,12 @@ describe("React TodoMVC", () => {
         cy.createDefaultTodos()
         cy.get('.todo-list li').should('have.length', 3)
     })
+
+    it('should append new items to the bottom of the list', () => {
+        cy.createDefaultTodos()
+        cy.get('.todo-list li').eq(0).find('label').should('contain', TODO_ITEM_ONE)
+        cy.get('.todo-list li').eq(1).find('label').should('contain', TODO_ITEM_TWO)
+        cy.get('.todo-list li').eq(2).find('label').should('contain', TODO_ITEM_THREE)
+        cy.get('.todo-count').contains('3 items left')
+    })
 })
