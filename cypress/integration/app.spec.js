5aa8e9290600841fcaeacf224e2219212f34fe86
edemuner <eduardood15@hotmail.com>
2022-04-06 13:47:47 -0300

    made another test to add multiple tasks and check if the correct number of tasks was added

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,24 @@
 describe("React TodoMVC", () => {
-    it("adds a single todo", () => {
+
+    const TODO_ITEM_ONE = "Buy Milk"
+    const TODO_ITEM_TWO = "Pay Rent"
+    const TODO_ITEM_THREE = "Puckup Dry Cleaning"
+
+
+    beforeEach(() => {
         cy.visit('http://localhost:8888')
-        cy.get('.new-todo').type('Buy Milk{enter}')
+    })
+
+    it("adds a single todo", () => {
+        cy.get('.new-todo').type(`${TODO_ITEM_ONE}{enter}`)
         cy.get('.todo-list li').should('have.length', 1)
+    })
+
+    it("adds three todos", () => {
+        cy.get('.new-todo')
+            .type(`${TODO_ITEM_ONE}{enter}`)
+            .type(`${TODO_ITEM_TWO}{enter}`)
+            .type(`${TODO_ITEM_THREE}{enter}`)
+        cy.get('.todo-list li').should('have.length', 3)
     })
 })
