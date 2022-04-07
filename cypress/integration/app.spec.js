15193c0976c9aed2aee7a1890b6175bf6342208d
edemuner <eduardood15@hotmail.com>
2022-04-07 09:30:41 -0300

    updated test should append new items to the bottom of the list to also use an alias

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -15,17 +15,18 @@ describe("React TodoMVC", () => {
         cy.get('.todo-list li').eq(0).find('label').should('contain', TODO_ITEM_ONE)
     })
 
-    it.only("adds three todos", () => {
+    it("adds three todos", () => {
         cy.get('.new-todo')
         cy.createDefaultTodos().as('todos')
         cy.get('@todos').should('have.length', 3)
     })
 
     it('should append new items to the bottom of the list', () => {
-        cy.createDefaultTodos()
+        cy.createDefaultTodos().as('todos')
         cy.get('.todo-list li').eq(0).find('label').should('contain', TODO_ITEM_ONE)
         cy.get('.todo-list li').eq(1).find('label').should('contain', TODO_ITEM_TWO)
         cy.get('.todo-list li').eq(2).find('label').should('contain', TODO_ITEM_THREE)
+        cy.get('@todos').should('have.length', 3)
         cy.get('.todo-count').contains('3 items left')
     })
 })
